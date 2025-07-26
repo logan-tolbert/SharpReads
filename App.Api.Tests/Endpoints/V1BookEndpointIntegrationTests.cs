78debae1333dcfb55a85749bbeef47ed7ff52aca
Logan <lodev205@gmail.com>
2025-07-26 11:10:20 -0500

    🧪 Add integration tests for V1BookEndpoints
    - Introduce `App.Api.Tests` project to the solution for testing.
    
    - Implement `V1BookEndpointIntegrationTests` and create an initial test to validate the behavior of a GET request to the `/v1/books` public endpoint.

---
+++ App.Api.Tests/Endpoints/V1BookEndpointIntegrationTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace App.Api.Tests.Endpoints;
+
+public class V1BookEndpointIntegrationTests(WebApplicationFactory<IApiMarker> factory)
+  : IClassFixture<WebApplicationFactory<IApiMarker>>
+{
+  [Fact]
+  public async Task GetPublicBooks_ReturnsSuccess()
+  {
+    var client = factory.CreateClient();
+    var response = await client.GetAsync("/v1/books", TestContext.Current.CancellationToken);
+    response.EnsureSuccessStatusCode();
+    var responseString = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
+    response.IsSuccessStatusCode.Should().BeTrue();
+  }
+}
